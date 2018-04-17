ca5d74cc05da21a356790d2b01da521f5fce5d60
ArizonaMangoJuice <isaelwebdeveloper@gmail.com>
2018-04-17 13:20:39 -0600

    created error handling and a 404 error handler

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,3 +1,4 @@
+'use strict';
 exports.logger = function(req, res, next){
     const date = new Date();
     console.log(`${date.toLocaleDateString()} ${date.toLocaleTimeString()} ${req.method} ${req.path}`);
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const data = require('./db/notes');
 const { PORT } = require('./config');
-const { logger } = require('./middleware/logger.js');
+const { logger } = require('./middleware/logger');
 const app = express();
 console.log('Hello Noteful!');
 
@@ -18,6 +18,24 @@ app.get('/api/notes', (req,res) => {
 
 app.get('/api/notes/:id', (req, res) => {
     res.json(data.find(item => item.id === Number(req.params.id)));
+});
+
+// app.get('/boom', (req, res, next) => {
+//     throw new Error('!boom');
+// });
+
+app.use(function (req, res, next) {
+    let err = new Error('Not Found');
+    err.status = 404;
+    res.status(404).json({ message: 'Not Found' });
+});
+
+app.use(function(err, req, res, next){
+    res.status(err.status || 500);
+    res.json({
+        message: err.message,
+        error: err,
+    });
 });
 
 app.listen(PORT, function() {
